5e43c4dfdd7b91eb7c6aeaa2b2e86cf021b0cf12
Kaloyan Ivanov <kaloyanivanov@hotmail.com>
2025-05-23 08:55:35 +0200

    Added dependency libraries
    - Added GLEW, GLFW, GLM and ASSIMP
    - Wrote a basic test script to test if everything builds correctly

--- Shadows/Shadows/main.cpp
+++ Shadows/Shadows/main.cpp
@@ -1,6 +1,42 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 int main()
 {
-	std::cout << "asd\n";
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if ( !glfwInit() )
+		return -1;
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow( 640, 480, "Hello World", NULL, NULL );
+	if ( !window ) {
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent( window );
+
+	if ( glewInit() != GLEW_OK ) {
+		std::cout << "Glew failed to init!" << std::endl;
+		return -1;
+	}
+
+	/* Loop until the user closes the window */
+	while ( !glfwWindowShouldClose( window ) ) {
+		/* Render here */
+		glClear( GL_COLOR_BUFFER_BIT );
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers( window );
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 }
